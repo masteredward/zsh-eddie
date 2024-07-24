b6d1d5873e684b981d116747b9ab7412037a841b
Eduardo Silva <eduardo.silva@sitecore.com>
2024-07-24 09:51:45 +0100

    Fixed zypper update alias

--- packagemanager.zsh
+++ packagemanager.zsh
@@ -3,7 +3,7 @@ source /etc/os-release
 case "$ID" in
   opensuse-tumbleweed)
     alias z='sudo zypper'
-    alias zup='sudo zypper up -y'
+    alias zup='sudo zypper dup -y'
     ;;
   fedora)
     alias d='sudo dnf'
