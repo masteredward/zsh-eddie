447a878dddfc939427d7af881b4bc65c32fed598
Eduardo Medeiros Silva <edu.medeiros.info@gmail.com>
2025-03-05 04:01:47 +0000

    Add docker_arm64_install function for installing QEMU on specific distributions

--- packagemanager.zsh
+++ packagemanager.zsh
@@ -10,3 +10,22 @@ case "$ID" in
     alias dup='sudo dnf update -y'
     ;;
 esac
+
+docker_arm64_install() {
+  case "$ID" in
+    opensuse-tumbleweed)
+      sudo zypper in -y \
+        qemu \
+        qemu-arm \
+        qemu-uefi-aarch64 \
+        qemu-linux-user
+      ;;
+    fedora)
+      sudo dnf install -y \
+        qemu \
+        qemu-arm \
+        qemu-uefi-aarch64 \
+        qemu-user
+      ;;
+  esac
+}
