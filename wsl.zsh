7d97bbf03cf6be82eea8b59b8946ad2b6c678304
Eduardo Medeiros Silva <edu.medeiros.info@gmail.com>
2025-07-12 22:28:58 +0100

    Updated files

--- wsl.zsh
+++ wsl.zsh
@@ -1,3 +1,3 @@
 if [[ "$(uname -r)" == *"microsoft-standard-WSL2" ]]; then
-    BROWSER="$(which librewolf.exe)"; export BROWSER
+    BROWSER="/mnt/c/Program Files (x86)/Microsoft/Edge/Application/msedge.exe"; export BROWSER
 fi
